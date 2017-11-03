cb6dc343f91ecccc0d8c2e6b49ab0a887e0d8bb5
Kenneth Heyen <kenneth.heyen@live.com>
2017-11-02 23:23:39 -0700

    Object oriented! ;)

---
+++ Project2/AutonomousSystems.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project2
+{
+    enum ASTYPE { ENTERPRISE, CONTENT, TRANSIT, UNKNOWN };
+
+    class AutonomousSystem
+    {
+        public ASTYPE type;
+
+        public AutonomousSystem()
+        {
+            type = ASTYPE.UNKNOWN;
+        }
+
+        public AutonomousSystem(string line)
+        {
+            if (line.ToUpper().Contains("CONTENT"))
+                type = ASTYPE.CONTENT;
+            else if (line.ToUpper().Contains("ENTERPRISE"))
+                type = ASTYPE.ENTERPRISE;
+            else if (line.ToUpper().Contains("TRANSIT"))
+                type = ASTYPE.TRANSIT;
+        }
+    }
+
+    class AutonomousSystems : List<AutonomousSystem>
+    {
+        public AutonomousSystems() : base() { }
+        public AutonomousSystems(string filename) : base()
+        {
+            AutonomousSystem autoSys = new AutonomousSystem();
+
+            foreach (string line in File.ReadAllLines(filename))
+            {
+                if (line[0] == '#')
+                    continue;
+
+                Add(new AutonomousSystem(line));
+            }
+        }
+
+        public int countType(ASTYPE type)
+        {
+            int count = 0;
+
+            foreach (AutonomousSystem autoSys in this)
+            {
+                if (autoSys.type == type)
+                    count++;
+            }
+
+            return count;
+        }
+    }
+}
--- Project2/Program.cs
+++ Project2/Program.cs
@@ -4,48 +4,60 @@ using System.IO;
 
 namespace Project2
 {
+
     class MainClass
     {
         public static void Main(string[] args)
         {
+            AutonomousSystems autoSystems;
 
             if (args.Length == 1)
             {
-                int CountASEnterprise = 0;
-                int CountASContent = 0;
-                int CountASTA = 0;
-                int CountBadInput = 0;
-                Console.WriteLine(args.Length);
-                string[] all_lines = System.IO.File.ReadAllLines(args[0]);
-                List<String> all_lines_filtered = new List<string>();
-                foreach (string line in all_lines)
-                {
-                    if (!(line[0] == '#'))
-                        all_lines_filtered.Add(line);
-                }
+                //int CountASEnterprise = 0;
+                //int CountASContent = 0;
+                //int CountASTA = 0;
+                //int CountBadInput = 0;
+                //Console.WriteLine(args.Length);
+                //string[] all_lines = System.IO.File.ReadAllLines(args[0]);
+                //List<String> all_lines_filtered = new List<string>();
+                //foreach (string line in all_lines)
+                //{
+                //    if (!(line[0] == '#'))
+                //        all_lines_filtered.Add(line);
+                //}
 
-                foreach (string filtered_line in all_lines_filtered)
-                {
-                    if (filtered_line.ToUpper().Contains("CONTENT"))
-                        CountASContent++;
-                    else if (filtered_line.ToUpper().Contains("ENTERPISE"))
-                        CountASEnterprise++;
-                    else if (filtered_line.ToUpper().Contains("TRANSIT"))
-                        CountASTA++;
-                    else
-                        CountBadInput++;
-                }
+                //foreach (string filtered_line in all_lines_filtered)
+                //{
+                //    if (filtered_line.ToUpper().Contains("CONTENT"))
+                //        CountASContent++;
+                //    else if (filtered_line.ToUpper().Contains("ENTERPISE"))
+                //        CountASEnterprise++;
+                //    else if (filtered_line.ToUpper().Contains("TRANSIT"))
+                //        CountASTA++;
+                //    else
+                //        CountBadInput++;
+                //}
 
-                Console.WriteLine("Number of Transit/Access: {0}", CountASTA);
-                Console.WriteLine("Number of Content: {0}", CountASContent);
-                Console.WriteLine("Number of Enterprise: {0}", CountASEnterprise);
-                Console.WriteLine("Number of Bad Input: {0}", CountBadInput);
+                autoSystems = new AutonomousSystems(args[0]);
 
+                //Console.WriteLine("Number of Transit/Access: {0}", CountASTA);
+                //Console.WriteLine("Number of Content: {0}", CountASContent);
+                //Console.WriteLine("Number of Enterprise: {0}", CountASEnterprise);
+                //Console.WriteLine("Number of Bad Input: {0}", CountBadInput);
 
+                Console.WriteLine("Number of Transit/Access: {0}", autoSystems.countType(ASTYPE.TRANSIT));
+                Console.WriteLine("Number of Content: {0}", autoSystems.countType(ASTYPE.CONTENT));
+                Console.WriteLine("Number of Enterprise: {0}", autoSystems.countType(ASTYPE.ENTERPRISE));
+                Console.WriteLine("Number of Bad Input: {0}", autoSystems.countType(ASTYPE.UNKNOWN));
 
                 var sw = new StreamWriter("./output.csv");
+
                 sw.WriteLine("Number of Transit/Access,Number Of Content, Number of Enterprise, Number of Bad Input");
-                sw.WriteLine(string.Format("{0},{1},{2},{3}", CountASTA, CountASContent, CountASEnterprise, CountBadInput));
+                sw.WriteLine(string.Format("{0},{1},{2},{3}",  
+                                            autoSystems.countType(ASTYPE.TRANSIT), 
+                                            autoSystems.countType(ASTYPE.CONTENT), 
+                                            autoSystems.countType(ASTYPE.ENTERPRISE), 
+                                            autoSystems.countType(ASTYPE.UNKNOWN)));
                 sw.Flush();
                 sw.Close();
 
